b617cb7a74c00ec1dc060a3734cb73540d929fd2
LearningBizz <learningbizz@gmail.com>
2021-10-09 20:00:10 +0200

    Promise chain fixed + added consolelogs

--- test2.js
+++ test2.js
@@ -1,33 +1,26 @@
-const { rejects } = require('assert');
-const open = require('open');
 const ical = require('ical')
 const fs = require('fs');
 const download = require('download');
 require('dotenv').config();
-const { resolve } = require('path');
-
-getNewIcal();
-//addOrUpdateNotionCalendar();
 
 
+getNewIcal().then(addOrUpdateNotionCalendar);
 
 
 async function getNewIcal() {
     try {
         const file = `${process.env.LINK_ICAL}`;
         const filePath = `${process.env.CURRENTDIR}`;
-        download(file,filePath)
-        .then(() => {
-            console.log('Download Completed');
+        await download(file,filePath)
+        console.log('Downloaded new Ical...');
+        fs.rename('./portada.ics', './newCalendar.ics', function (err) {
+            if (err)
+                console.log('ERROR: ' + err);
         })
-        .then(() => {
-            fs.rename('./portada.ics', './newCalendar.ics', function(err) {
-                if ( err ) console.log('ERROR: ' + err);
-            });
-        });
+        console.log('Changed new Ical name to newCalendar...\n')
     }
     catch(error) {
-        console.log(error);
+        console.log('ERROR: ' + error);
     }
 }
 
@@ -35,22 +28,28 @@ async function addOrUpdateNotionCalendar() {
     try {
         const newIcal = ical.parseFile('./newCalendar.ics')
         const oldIcal = ical.parseFile('./oldCalendar.ics')
+        let counterEventsAdded = 0;
         let sub = {};
         for (let k in newIcal) {
             //if the event is not in the old calendar OR its modified in the new calendar
             if(!(k in oldIcal) || (await checkIcalObjectUpdate(newIcal[k],oldIcal[k]) == false)) {
+                ++counterEventsAdded;
+                console.log(newIcal[k].summary + ' event was found...')
                 sub[k] = newIcal[k];
-                console.log(sub[k])
+                //console.log(sub[k]);
             }
-            
         }
+        if(counterEventsAdded == 1) 
+            console.log('\nA total of ' + counterEventsAdded + ' event was created/updated...')
+        else
+        console.log('\nA total of ' + counterEventsAdded + ' events were created/updated...')
     }
     catch(error) {
-        console.log(error);
+        console.log('ERROR: ' + error);
     }
 
-
 }
+
 
 async function checkIcalObjectUpdate(ical1, ical2) {
     return (ical1.summary === ical2.summary) &&
@@ -58,3 +57,10 @@ async function checkIcalObjectUpdate(ical1, ical2) {
            (ical1.end.toISOString() === ical2.end.toISOString());
 }
   
+/**
+ * Add an iCal event to the Notion database.
+ * @param {*} icalEvent 
+ */
+async function addEventToNotionDB(icalEvent) {
+
+}
